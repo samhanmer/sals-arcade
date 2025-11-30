e17e6b1aac85930fcd1e67c6189cdc198f4419af
samhanmer <SamHanmer@hotmail.com>
2025-11-30 16:22:04 -0700

    Updated file structure with an Assets folder and added Mission.html

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+function greetUser() {
+    const hour = new Date().getHours();
+    let message = "Hi there!"
+
+    if (hour < 12) message = "Good morning!";
+    if (hour < 18) message = "Good afternoon!";
+    else message = "Good evening!";
+
+    document.getElementById("greet").innerText = message;
+}
+window.onload = greetUser; /* JS to greet the user based on time of day. Only used in index*/
